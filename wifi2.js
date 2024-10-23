1bb17b4bd7f96bb417c8e51fe857abb029f2a944
x01-889 <hfg05808@gmail.com>
2024-10-23 14:39:53 +0300

    Add files via upload

---
+++ wifi2.js
@@ -0,0 +1,5 @@
+// التأكد من أن الزر يتفاعل مع الضغط
+document.getElementById('learn-more-btn').addEventListener('click', function() {
+    // الانتقال إلى صفحة أخرى
+    window.location.href = 'https://www.example.com'; // استبدل هذا بالرابط الفعلي الذي تريد الانتقال إليه
+});
